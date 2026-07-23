f8d5bf39062cb631f38b7de10ef29c8de5742dbe
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-23 10:18:32 +0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -6,7 +6,7 @@ export default {
   // Server
   port: process.env.PORT || 5000,
   nodeEnv: process.env.NODE_ENV || 'development',
-  frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3000',
+  frontendUrl: process.env.FRONTEND_URL || 'https://alpha-steel-ten.vercel.app',
 
   // JWT
   jwtSecret: process.env.JWT_SECRET,
